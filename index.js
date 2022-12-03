bf07124dbcf5488b07e0ffda5afb384da96557a3
Oakley Hall <ohall@walmart.com>
2022-12-03 15:01:48 -0500

    page update

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 import njk from './njk.js';
 
+const BOTTOM_Y = 1;
+const TOP_Y = BOTTOM_Y + 1.5;
+const ROTATION = 45;
+const SCREEN_WIDTH = 8;
+const SCREEN_HEIGHT = 1.5;
+const Z_OFFSET = -3.5
+const X_OFFSET = SCREEN_WIDTH/2;
+const radius = 5.7;
+
 const model = {
+	common: {
+		height: SCREEN_HEIGHT,
+		width: SCREEN_WIDTH,
+	},
 	charts:  [
 		// bottom row
-		{ x: 2.6,	y: 1.5,	z: -1.5,	rotation: -45,src: 1 },
-		{ x: 0.0,	y: 1.5,	z: -2.5,	rotation: 0,	src: 2 },
-		{ x: -2.6,y: 1.5,	z: -1.5,	rotation: 45,	src: 3 },
+		{ x: X_OFFSET,	y: BOTTOM_Y,	z: Z_OFFSET,		rotation: -ROTATION,	src: 1 },
+		{ x: 0.0,				y: BOTTOM_Y,	z: Z_OFFSET-1,	rotation: 0,					src: 2 },
+		{ x: -X_OFFSET,	y: BOTTOM_Y,	z: Z_OFFSET,		rotation: ROTATION,		src: 3 },
 		// top row
-		{ x: 2.6,	y: 3,	z: -1.5,	rotation: -45,src: 4 },
-		{ x: 0.0,	y: 3,	z: -2.5,	rotation: 0,	src: 5 },
-		{ x: -2.6,y: 3,	z: -1.5,	rotation: 45,	src: 6 }
+		{ x: X_OFFSET,	y: TOP_Y,	z: Z_OFFSET,		rotation: -ROTATION,src: 4 },
+		{ x: 0.0,				y: TOP_Y,	z: Z_OFFSET-1,	rotation: 0,				src: 5 },
+		{ x: -X_OFFSET,	y: TOP_Y,	z: Z_OFFSET,		rotation: ROTATION,	src: 6 }
 	],
-	radius: 5.7
+	radius
 }
+
 window.onload = function() {
 	const $e = document.querySelector('#ops-displays');
 	const uri = 'https: //goquotes-api.herokuapp.com/api/v1/random?count=1';
